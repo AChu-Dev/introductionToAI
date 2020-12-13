baabf6834771e69e16a609ae4173715084747853
RomanDom88 <4romad67@solent.ac.uk>
2020-12-13 14:59:52 +0000

    py

---
+++ ai.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import time
+
+print('TensorFlow version: ',tf.__version__)
+
+start_time = time.time()
+
+print(tf.reduce_sum(tf.random.normal([1000, 1000])))
+
+print(f"{(time.time() - start_time)} Compilation Time")
