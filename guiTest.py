da602c88e557e15549e4d401fbc188d5b26d297b
RomanDom88 <4romad67@solent.ac.uk>
2020-12-18 13:14:31 +0000

    changes

--- guiTest.py
+++ guiTest.py
@@ -26,12 +26,12 @@ class InterfaceGUI:
 
         program.add_command(label='Restart')
 
-        # self.guiBuilder(window)
-        frame0 = ttk.Frame(window)
-        frame0.place(x=5, y=5, height=180, width=180)
+        self.guiBuilder(window)
+        # frame0 = ttk.Frame(window)
+        # frame0.place(x=5, y=5, height=180, width=180)
 
-        field000 = ttk.Entry(frame0, justify=CENTER, font=("Cambria", "32", "bold"))
-        field000.place(x=0, y=0, height=60, width=60)
+        # field000 = ttk.Entry(frame0, justify=CENTER, font=("Cambria", "32", "bold"))
+        # field000.place(x=0, y=0, height=60, width=60)
 
 
         # Frames, 9x9 grid # 3 values there
@@ -54,13 +54,15 @@ class InterfaceGUI:
     def guiBuilder(self, window):
         frames = []
         fields = []
-
         frame_diff = 189  # dont forget 5 px border
         field_diff = 61
 
+        # define frame names
         for x in range(9):
-            frames.append("frames" + str(x))
-        
+            frames.append("frame" + str(x))
+            self.field_names(x, fields)
+        self.field_names(x, fields)
+        print(fields)
         varX = 0
         varY = 0
         for array_num in range(9):
@@ -73,6 +75,15 @@ class InterfaceGUI:
                 varX = 0
                 varY += 1
 
+    def field_names(self, frame_num, fields):
+        left_pointer = 0
+        level_pointer = 0
+        while left_pointer == 2 and level_pointer == 2:
+            fields.append("field" + str(frame_num) + str(level_pointer) + str(left_pointer))
+            left_pointer += 1
+            if left_pointer == 2:
+                left_pointer = 0
+                level_pointer += 1
         # for x in range(3):
         #    for y in range(3):
         #        fields.append("frame" + str(x) + str(y))
@@ -85,6 +96,9 @@ class InterfaceGUI:
         #    frames[z].config(height=180, width=180)
         # if list(frames[z])[5]:
         #    frames[z].place(x=frame_value0+z*10, y=frame_value0+z*10)
+    def object_placement(self, varX, varY, varHeight, varWidth, window, **kwargs):
+        pass
+
 
 
 if __name__ == '__main__':  # for testing purposes it will stay here
