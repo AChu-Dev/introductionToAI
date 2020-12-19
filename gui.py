abd48095c7f4a757b246f01b75ae67db37ab215b
RomanDom88 <4romad67@solent.ac.uk>
2020-12-19 15:10:30 +0000

    updates

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@ def main():
     root.mainloop()
 
 
-def loadData(data):
+def load_data(data):
     pass
 
 
@@ -30,9 +30,9 @@ class InterfaceGUI:
 
         program.add_command(label='Restart')
 
-        self.guiBuilder(window)
+        self.__guiBuilder(window)
 
-    def guiBuilder(self, window):
+    def __guiBuilder(self, window):
         frames = []
         fields = []
 
@@ -40,25 +40,25 @@ class InterfaceGUI:
 
         for x in range(9):
             frames.append("frame" + str(x))
-            self.field_names(x, fields)
+            self.__field_names(x, fields)
 
         var_x = 0
         var_y = 0
         for array_num in range(9):
             frames[array_num] = ttk.Frame(window)
             frames[array_num].place(x=(189 * var_x) + 5, y=(189 * var_y) + 5, height=180, width=180)
-            self.fields_manager(frames[array_num], fields)
+            self.__fields_manager(frames[array_num], fields)
             var_x += 1
             if var_x == 3:
                 var_x = 0
                 var_y += 1
 
-    def field_names(self, frame_num, fields):
+    def __field_names(self, frame_num, fields):
         for x in range(3):
             for y in range(3):
                 fields.append("field" + str(frame_num) + str(x) + str(y))
 
-    def fields_manager(self, frame_num, fields):
+    def __fields_manager(self, frame_num, fields):
         var_x = 0
         var_y = 0
         for array_num2 in range(9):
@@ -72,7 +72,7 @@ class InterfaceGUI:
     def updateData(self, x, y, value):
         pass
 
-    def restart(self):
+    def clear(self):
         pass
 
 
