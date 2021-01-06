653cc37de95e7022fba3a1a130c47fa3db3cf07e
Adam Chu <adamchu2001@gmail.com>
2021-01-06 23:28:56 +0000

    solver.py added grid3Constraints

--- solver.py
+++ solver.py
@@ -1,6 +1,7 @@
 import numpy
 
 
+
 def sudokuSolver(array):
     # try:
     for x in range(len(array)):
@@ -10,6 +11,10 @@ def sudokuSolver(array):
                     if checkValues(array, x, y, value):
                         array[x][y] = value
                         # backtracking
+        if grid3Constraints(x):
+            print("Grid Follows 3*3 Sudoku Rules")
+        else:
+            print("Grid Does Not Follow 3*3 Sudoku Rules")
     print(array)
 
 
@@ -38,6 +43,69 @@ def checkValues(array, x, y, value):
                 return False
     return True
 
+    def grid3Constraints(array):
+        print("Checking 3 *3 Grid Constraints\n")
+        print(str(array[0]) + "\t" + str(array[1]) + "\t" +  str(array[2]) + "\n")
+        print(str(array[3]) + "\t" + str(array[4]) + "\t" +  str(array[5]) + "\n")
+        print(str(array[6]) + "\t" + str(array[7]) + "\t" +  str(array[8]) + "\n")
+        horiz1 = 0
+        horiz2 = 1
+        horiz3 = 2
+        verti1 = 0
+        verti2 = 3
+        verti3 = 6
+        counter = 1
+        if checkUniqueRange(array) == False:
+            return False
+        else:
+            # Diagonal Constraints dont change so they dont need iteration
+            if (array[6] + array[4] + array[2] == 15) and (array[0] + array[4] + array[8] == 15):
+                print("Passed Both Diagonal Constraints")
+                for x in range(3):
+                    if (array[horiz1] + array[horiz2] + array[horiz3] == 15) and (
+                            array[verti1] + array[verti2] + array[verti3] == 15):
+                        print("Passed vertical & horizontal test: " + str(counter))
+                        horiz1 += 3
+                        horiz2 += 3
+                        horiz3 += 3
+                        verti1 += 1
+                        verti2 += 1
+                        verti3 += 1
+                        counter += 1
+                    else:
+                        print("Failed on Horizontal/Vertical: " + str(counter))
+                        return False
+                print("Passed Tests")
+                return True
+
+            else:
+                print("Numbers in Diagonals do not add up to 15")
+                return False
+
+def checkUniqueRange(array):
+    print("Checking Numbers for being in the correct format\n")
+    if (len(array) == 9):
+        
+        print("array in: " + str(array))
+        arrayToSet = set(array)
+        print("Set array: " + str(arrayToSet))
+        print("length of array: " + str(len(array)))
+        print("length of set: " + str(len(arrayToSet)))
+        for x in array:
+            value = int(x)
+            print("value in array: " + str(value))
+            if (1 <= value <= 9) and (len(array) == len(arrayToSet)):
+                print("Succeded as Numbers are in Range\n")
+                return True
+            else:
+                print('Failed due to numbers being out of range or not all numbers being distinct')
+                return False
+            # print('Failed Test')
+            # return False
+    else:
+        print("Failed due to wrong number count")
+
+        return False
 
 if __name__ == '__main__':
     array = numpy.array([[0, 0, 2, 0, 1, 5, 0, 7, 8],
